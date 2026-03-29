54c00e733664a57277b33482d156dcc13cdad9c0
Ossama Nasser <ossnass@gmail.com>
2026-03-29 07:35:46 +0300

    Initial

---
+++ setup/shiki.ts
@@ -0,0 +1,14 @@
+import { defineShikiSetup
+ } from '@slidev/types'
+
+export default defineShikiSetup
+(() => {
+  return {
+    themes: {
+      dark: 'solarized-dark',
+      light: 'solarized-light',
+    },
+  }
+})
+
+
---
+++ slidev-theme-penguin-rtl/components/corner-curves/CornerCurves.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import CurveBig from './assets/curve-big.svg'
+import CurveSmall from './assets/curve-small.svg'
+</script>
+
+<template>
+  <div>
+    <div class="relatvie">
+        <CurveBig class="absolute bottom-0 right-0 curve-revese"/>
+        <CurveSmall class="absolute bottom-0 right-40 opacity-40" />
+    </div>
+  </div>
+</template>
